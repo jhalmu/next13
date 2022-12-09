8b9387d3f1810942024e4f008393a65f4b7ce40d
Juha Halmu <jhalmu@users.noreply.github.com>
2022-12-09 11:08:01 +0200

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import Footer from './footer'
+import './globals.css'
+import Header from './header'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <Header />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html >
+  )
+}
